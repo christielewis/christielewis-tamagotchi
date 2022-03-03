442fd8042498afcf1851df160051a26f9d18b4c3
Christie Lewis <christielewis@Christies-MacBook-Air.local>
2022-03-03 15:37:43 -0500

    added basic increment/decrement basd on click events

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,52 @@
-console.log("Does this work?");
+const feedButton = document.getElementById("feed");
+const playButton = document.getElementById("play");
+const napButton = document.getElementById("nap");
+
+let hungerCount = document.getElementById("hunger");
+let boredomCount = document.getElementById("boredom");
+let sleepyCount = document.getElementById("sleepy");
+
+// console.log(hungerCount.innerText);
+
+let hungerNum = 0;
+let boredomNum = 0;
+let sleepyNum = 0;
+// console.log(hungerNum);
+
+const addOneHunger = () => {
+    hungerNum++;
+    hungerCount.innerText = hungerNum;
+}
+const addOneBoredom = () => {
+    boredomNum++;
+    boredomCount.innerText = boredomNum;
+}
+const addOneSleepy = () => {
+    sleepyNum++;
+    sleepyCount.innerText = sleepyNum;
+}
+
+const subOneHunger = () => {
+    hungerNum--;
+    hungerCount.innerText = hungerNum;
+    // console.log(hungerNum);
+    addOneBoredom();
+    addOneSleepy();
+}
+const subOneBoredom = () => {
+    boredomNum--;
+    boredomCount.innerText = boredomNum;
+    addOneHunger();
+    addOneSleepy();
+}
+const subOneSleepy = () => {
+    sleepyNum--;
+    sleepyCount.innerText = sleepyNum;
+    addOneHunger();
+    addOneBoredom();
+}
+
+
+feedButton.addEventListener('click', subOneHunger);
+playButton.addEventListener('click', subOneBoredom);
+napButton.addEventListener('click', subOneSleepy);
