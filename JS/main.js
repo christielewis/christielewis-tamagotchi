d3fb0d3afb598ea945b917605f270a6f4765f36f
Christie Lewis <christielewis@Christies-MacBook-Air.local>
2022-03-03 18:38:14 -0500

    added gifs on both pgs; gifs change according to button clicked

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,28 @@
+const removeGif = () => {
+    const imgEl = document.querySelector("img");
+    const tamaGameId = document.querySelector("#tama-gifs");
+    tamaGameId.removeChild(imgEl);
+}
+
+const addFeedGif = () => {
+    removeGif();
+    let feedGif = document.createElement("img");
+    feedGif.setAttribute("src", "https://media4.giphy.com/media/iWkHDNtcHpB5e/giphy.gif?cid=ecf05e47updfo76ke3hr78jvon8985lt2gpc1jyrxr3u88nf&rid=giphy.gif&ct=g");
+    document.querySelector("#tama-gifs").appendChild(feedGif);
+}
+const addPlayGif = () => {
+    removeGif();
+    let playGif = document.createElement("img");
+    playGif.setAttribute("src", "https://media0.giphy.com/media/jEyKIvmt0BgLC/giphy.gif?cid=ecf05e47f8zu34sipklh0bq0miebtdrfleniue9vt7xjx056&rid=giphy.gif&ct=g");
+    document.querySelector("#tama-gifs").appendChild(playGif);
+}
+const addNapGif = () => {
+    removeGif();
+    let napGif = document.createElement("img");
+    napGif.setAttribute("src", "https://media3.giphy.com/media/JxFmWGrmynlCg/giphy.gif?cid=ecf05e47bgjw672bk7y3o0u2i7le279zl3m43uhq8pszz1oe&rid=giphy.gif&ct=g");
+    document.querySelector("#tama-gifs").appendChild(napGif);
+}
+
 const feedButton = document.getElementById("feed");
 const playButton = document.getElementById("play");
 const napButton = document.getElementById("nap");
@@ -6,46 +31,43 @@ let hungerCount = document.getElementById("hunger");
 let boredomCount = document.getElementById("boredom");
 let sleepyCount = document.getElementById("sleepy");
 
-// console.log(hungerCount.innerText);
-
 let hungerNum = 0;
 let boredomNum = 0;
 let sleepyNum = 0;
-// console.log(hungerNum);
 
 const addOneHunger = () => {
     hungerNum++;
     hungerCount.innerText = hungerNum;
-}
+};
 const addOneBoredom = () => {
     boredomNum++;
     boredomCount.innerText = boredomNum;
-}
+};
 const addOneSleepy = () => {
     sleepyNum++;
     sleepyCount.innerText = sleepyNum;
-}
+};
 
 const subOneHunger = () => {
     hungerNum--;
     hungerCount.innerText = hungerNum;
-    // console.log(hungerNum);
+    addFeedGif();
     addOneBoredom();
-    addOneSleepy();
-}
+};
 const subOneBoredom = () => {
     boredomNum--;
     boredomCount.innerText = boredomNum;
-    addOneHunger();
+    addPlayGif();
     addOneSleepy();
-}
+};
 const subOneSleepy = () => {
+    // removeGif();
     sleepyNum--;
     sleepyCount.innerText = sleepyNum;
+    // removeGif();
+    addNapGif();
     addOneHunger();
-    addOneBoredom();
-}
-
+};
 
 feedButton.addEventListener('click', subOneHunger);
 playButton.addEventListener('click', subOneBoredom);
